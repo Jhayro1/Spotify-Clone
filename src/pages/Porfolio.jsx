ea97a8bd94b76c87fffad22aef982c702cb2c353
Jhayro <jhayro2017@gmail.com>
2022-12-13 02:38:07 -0500

    Spotify-Clone

---
+++ src/pages/Porfolio.jsx
@@ -0,0 +1,21 @@
+import {BsGithub,BsPerson} from 'react-icons/bs';
+const Porfolio = () => {
+  return (
+    <div className='flex flex-row justify-around mt-20'>
+        <div className='flex flex-col w-[200px] p-4 bg-white/5 bg-opacity-80 
+            backdrop-blur-sm animate-slideup rounded-lg cursor-pointer items-center py-7'>
+            <BsGithub className='text-white text-[50px]'/>
+            <a className="text-white cursor-pointer pt-5"href="">See Code</a>
+
+        </div>
+        <div className='flex flex-col w-[200px] p-4 bg-white/5 bg-opacity-80 
+            backdrop-blur-sm animate-slideup rounded-lg cursor-pointer items-center py-7'>
+              <BsPerson className='text-white text-[50px]'/>
+            <a className="text-white cursor-pointer pt-5" href="">See Porfolio</a>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Porfolio
