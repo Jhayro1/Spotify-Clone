b31fa15602eb19bfc44f8a5cee5614fcf20e5f93
Jhayro <jhayro2017@gmail.com>
2022-12-13 02:48:37 -0500

    Links - code

--- src/pages/Porfolio.jsx
+++ src/pages/Porfolio.jsx
@@ -5,7 +5,7 @@ const Porfolio = () => {
         <div className='flex flex-col w-[200px] p-4 bg-white/5 bg-opacity-80 
             backdrop-blur-sm animate-slideup rounded-lg cursor-pointer items-center py-7'>
             <BsGithub className='text-white text-[50px]'/>
-            <a className="text-white cursor-pointer pt-5"href="">See Code</a>
+            <a className="text-white cursor-pointer pt-5" href="https://github.com/Jhayro1/Spotify-Clone" >See Code</a>
 
         </div>
         <div className='flex flex-col w-[200px] p-4 bg-white/5 bg-opacity-80 
